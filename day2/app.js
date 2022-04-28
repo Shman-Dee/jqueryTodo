9941ad7d4349466631d16046955a723437795460
Andrew McInally <realfunnyestate@yahoo.com>
2022-04-27 20:48:19 -0600

    jquery is fun

--- day2/app.js
+++ day2/app.js
@@ -1,4 +1,12 @@
-let $input = $('input');
-let $button = $('button');
-let $ul = $('ul');
+let $input = $("input");
+let $button = $("button");
+let $ul = $("ul");
 
+console.log($button);
+
+$button.on("click", (e) => {
+  console.log(e);
+  let $li = $("<li>").text($input.val());
+  $ul.append($li);
+  $input.val("");
+});
