16ef314aaecfb10669ab49b8e464fa09ab420d3b
Andrew McInally <realfunnyestate@yahoo.com>
2022-04-30 11:33:25 -0600

    huh?

--- day3.a/README.md
+++ day3.a/README.md
@@ -1,3 +1,4 @@
 # react express sql
 
 stuff i am mor familiar with. python was a bit much to handle.
+na na an
--- day3.a/server/server.js
+++ day3.a/server/server.js
@@ -1,10 +1,34 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connection = require("./db");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.listen(3001, () => {
-  console.log('listening on port 3001 dad')
-})
+app.get("/tasks", (req, res) => {
+  res.send("list of all task");
+});
+
+app.post("/addTask", (req, res) => {
+  const ADD_QUERY = `INSERT INTO react_todos.tasks (task) VALUES ('${req.body.task}')`;
+  
+  connection.query(ADD_QUERY, (err) => {
+    if (err) console.log(err)
+    else res.send('task has been added successfully')
+  });
+  
+});
+
+app.delete("/deleteTask", (req, res) => {
+  res.send("deleted task");
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000 dad");
+});
+
+
+//trying to link it up
